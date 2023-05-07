c112fd7ec01431c9ef1f5d99844deaf5d1fb7fc9
mxdnxght <cloudyfg@gmail.com>
2023-05-07 23:10:17 +0300

    Implemented API controller for TrainRepository (TrainController).

---
+++ TrainManagementSystem/Controllers/TrainController.cs
@@ -0,0 +1,53 @@
+﻿using BankSystem7.Extensions;
+using BankSystem7.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mysqlx.Crud;
+using TrainManagementSystem.Extensions;
+using TrainManagementSystem.Models;
+
+namespace TrainManagementSystem.Controllers
+{
+    [Route("api/trains")]
+    [ApiController]
+    public class TrainController : ControllerBase
+    {
+        private readonly IRepository<Train> _trainRepository;
+        public TrainController(IRepository<Train> trainRepository)
+        {
+            _trainRepository = trainRepository;
+        }
+
+        [HttpGet]
+        public string All()
+        {
+            return _trainRepository.All.Serialize();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(Guid id)
+        {
+            return _trainRepository.Get(x => x.Id == id).Serialize();
+        }
+
+        [HttpPost]
+        public void Create(Train train)
+        {
+            _trainRepository.Create(train);
+        }
+
+        [HttpPut]
+        public void Update(Guid id, Train train)
+        {
+            var updOrder = _trainRepository.Get(x => x.Id == id);
+            _trainRepository.Update(train.SetValuesTo(updOrder));
+        }
+
+        [HttpDelete]
+        public void Delete(Guid id)
+        {
+            var train = _trainRepository.Get(x => x.Id == id);
+            _trainRepository.Delete(train);
+        }
+    }
+}
--- TrainManagementSystem/Program.cs
+++ TrainManagementSystem/Program.cs
@@ -1,3 +1,12 @@
+using BankSystem7.Services.Configuration;
+using BankSystem7.Services;
+using Microsoft.EntityFrameworkCore;
+using TrainManagementSystem.Extensions;
+using TrainManagementSystem.Services.Configuration;
+using TrainManagementSystem.Data;
+
+const string DatabaseName = "Test2";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTrainManagementSystem(GetOptions());
 
 var app = builder.Build();
 
@@ -23,3 +33,21 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+ConfigurationOptions GetOptions()
+{
+    return new ConfigurationOptions
+    {
+        EnsureCreated = false,
+        EnsureDeleted = false,
+        DatabaseName = DatabaseName,
+        OperationOptions = new OperationServiceOptions
+        {
+            DatabaseName = DatabaseName,
+        },
+        Contexts = new Dictionary<DbContext, ModelConfiguration?>
+        {
+            { new TrainContext(), new TrainManagementModelConfiguration() },
+        }
+    };
+}
