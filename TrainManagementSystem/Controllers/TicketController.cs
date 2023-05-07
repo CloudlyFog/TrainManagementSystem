1d8406d32359a647256a0e9feafd3bfb76dacba3
mxdnxght <cloudyfg@gmail.com>
2023-05-07 23:52:36 +0300

    Implemented API controller for TicketRepository (TicketController).

---
+++ TrainManagementSystem/Controllers/TicketController.cs
@@ -0,0 +1,56 @@
+﻿using BankSystem7.Extensions;
+using BankSystem7.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using TrainManagementSystem.Extensions;
+using TrainManagementSystem.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TrainManagementSystem.Controllers;
+
+[Route("api/tickets")]
+[ApiController]
+public class TicketController : ControllerBase
+{
+    private readonly IRepository<Ticket> _ticketRepository;
+    public TicketController(IRepository<Ticket> ticketRepository)
+    {
+        _ticketRepository = ticketRepository;
+    }
+
+    [HttpGet]
+    public string All()
+    {
+        return _ticketRepository.All.Serialize();
+    }
+
+
+    [HttpGet("{id}")]
+    public string Get(Guid id)
+    {
+        return _ticketRepository.Get(x => x.Id == id).Serialize();
+    }
+
+
+    [HttpPost]
+    public void Create(Ticket ticket)
+    {
+        _ticketRepository.Create(ticket);
+    }
+
+
+    [HttpPut("{id}")]
+    public void Update(Guid id, Ticket ticket)
+    {
+        var updTicket = _ticketRepository.Get(x => x.Id == id);
+        _ticketRepository.Update(ticket.SetValuesTo(ticket));
+    }
+
+
+    [HttpDelete("{id}")]
+    public void Delete(Guid id)
+    {
+        var ticket = _ticketRepository.Get(x => x.Id == id);
+        _ticketRepository.Delete(ticket);
+    }
+}
--- TrainManagementSystem/Controllers/TrainController.cs
+++ TrainManagementSystem/Controllers/TrainController.cs
@@ -6,48 +6,47 @@ using Mysqlx.Crud;
 using TrainManagementSystem.Extensions;
 using TrainManagementSystem.Models;
 
-namespace TrainManagementSystem.Controllers
+namespace TrainManagementSystem.Controllers;
+
+[Route("api/trains")]
+[ApiController]
+public class TrainController : ControllerBase
 {
-    [Route("api/trains")]
-    [ApiController]
-    public class TrainController : ControllerBase
+    private readonly IRepository<Train> _trainRepository;
+    public TrainController(IRepository<Train> trainRepository)
     {
-        private readonly IRepository<Train> _trainRepository;
-        public TrainController(IRepository<Train> trainRepository)
-        {
-            _trainRepository = trainRepository;
-        }
+        _trainRepository = trainRepository;
+    }
 
-        [HttpGet]
-        public string All()
-        {
-            return _trainRepository.All.Serialize();
-        }
+    [HttpGet]
+    public string All()
+    {
+        return _trainRepository.All.Serialize();
+    }
 
-        [HttpGet("{id}")]
-        public string Get(Guid id)
-        {
-            return _trainRepository.Get(x => x.Id == id).Serialize();
-        }
+    [HttpGet("{id}")]
+    public string Get(Guid id)
+    {
+        return _trainRepository.Get(x => x.Id == id).Serialize();
+    }
 
-        [HttpPost]
-        public void Create(Train train)
-        {
-            _trainRepository.Create(train);
-        }
+    [HttpPost]
+    public void Create(Train train)
+    {
+        _trainRepository.Create(train);
+    }
 
-        [HttpPut]
-        public void Update(Guid id, Train train)
-        {
-            var updOrder = _trainRepository.Get(x => x.Id == id);
-            _trainRepository.Update(train.SetValuesTo(updOrder));
-        }
+    [HttpPut]
+    public void Update(Guid id, Train train)
+    {
+        var updOrder = _trainRepository.Get(x => x.Id == id);
+        _trainRepository.Update(train.SetValuesTo(updOrder));
+    }
 
-        [HttpDelete]
-        public void Delete(Guid id)
-        {
-            var train = _trainRepository.Get(x => x.Id == id);
-            _trainRepository.Delete(train);
-        }
+    [HttpDelete]
+    public void Delete(Guid id)
+    {
+        var train = _trainRepository.Get(x => x.Id == id);
+        _trainRepository.Delete(train);
     }
 }
