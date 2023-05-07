8c2f93122b2b72042849a92fc9362c1fe431f9e2
mxdnxght <cloudyfg@gmail.com>
2023-05-07 19:57:41 +0300

    Implemented services to add TrainManagementSystem into DI.

---
+++ TrainManagementSystem/Extensions/SerializerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace TrainManagementSystem.Extensions;
+
+public static class SerializerExtensions
+{
+    public static string Serialize<TObject>(this TObject obj)
+    {
+        return JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        });
+    }
+}
